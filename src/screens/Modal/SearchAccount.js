80d01831b73612ed8c01509cb0b219bb0def970a
Agung1606 <agngsptra928@gmail.com>
2023-07-05 13:44:58 +0700

    search engine with firestore

--- src/screens/Modal/SearchAccount.js
+++ src/screens/Modal/SearchAccount.js
@@ -1,12 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  Pressable,
-  FlatList,
-} from "react-native";
+import { View, Text, TextInput, Pressable, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AntDesign } from "@expo/vector-icons";
 
@@ -16,24 +9,26 @@ import { setUserSearchHistory } from "../../redux/globalSlice";
 import { styled } from "nativewind";
 const StyledPressable = styled(Pressable);
 
-import { PROFILE } from "../../constant";
 import { modalPopupConfig } from "../../hooks";
 import { ConfirmModal, Avatar } from "../../components";
 
-const SearchBar = ({ goBack, query, setQuery }) => (
+import { FIREBASE_FIRESTORE } from "../../../firebaseConfig";
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+
+const SearchBar = ({ goBack, searchQuery, setSearchQuery }) => (
   <View
     className={`flex-row justify-between items-center py-2 px-4 border-b border-gray-600`}
   >
-    <TouchableOpacity onPress={goBack}>
+    <Pressable onPress={goBack}>
       <AntDesign name="arrowleft" size={22} />
-    </TouchableOpacity>
+    </Pressable>
     <View className="flex-1 px-4 py-2 ml-6">
       <TextInput
         placeholder="Cari akun"
         className="font-InterMedium text-gray-600"
         autoFocus={true}
-        value={query}
-        onChangeText={(text) => setQuery(text)}
+        value={searchQuery}
+        onChangeText={(text) => setSearchQuery(text)}
       />
     </View>
   </View>
@@ -42,17 +37,7 @@ const SearchBar = ({ goBack, query, setQuery }) => (
 const SearchAccount = ({ navigation }) => {
   const dispatch = useDispatch();
 
-  const goBack = () => navigation.goBack();
-  const goToProfile = (item) => {
-    navigation.navigate("VisitProfileScreen", {
-      param: { username: item.username, userId: item.id },
-    });
-    handleHistory(item);
-  };
-
-  const { isModalOpen, openModal, closeModal } = modalPopupConfig();
-
-  const [query, setQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
   const [users, setUsers] = useState([]);
   const userSearchHistory = useSelector(
     (state) => state.global.userSearchHistory
@@ -67,21 +52,43 @@ const SearchAccount = ({ navigation }) => {
     }
   };
 
-  useEffect(() => {
-    if (query) {
-      const filtered = PROFILE.filter((user) =>
-        user.username.toLowerCase().includes(query.toLowerCase())
-      );
-      setUsers(filtered);
-    }
+  const goBack = () => navigation.goBack();
+  const goToProfile = (item) => {
+    navigation.navigate("VisitProfileScreen", {
+      param: { username: item.username, userId: item.id },
+    });
+    handleHistory(item);
+  };
 
-    return () => setUsers([]);
-  }, [query]);
+  const { isModalOpen, openModal, closeModal } = modalPopupConfig();
+
+  useEffect(() => {
+    const q = query(
+      collection(FIREBASE_FIRESTORE, "users"),
+      where("username", ">=", searchQuery)
+    );
+    onSnapshot(q, (response) => {
+      setUsers(
+        response.docs.map((doc) => {
+          return {
+            id: doc.id,
+            username: doc.data().username,
+            name: doc.data().name,
+            profile: doc.data().profile,
+          };
+        })
+      );
+    });
+  }, [searchQuery]);
 
   return (
     <SafeAreaView className="flex-1 ">
-      <SearchBar goBack={goBack} query={query} setQuery={setQuery} />
-      {users && query ? (
+      <SearchBar
+        goBack={goBack}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
+      {users && searchQuery ? (
         <FlatList
           data={users}
           renderItem={({ item }) => (
@@ -110,9 +117,9 @@ const SearchAccount = ({ navigation }) => {
                   <Text className="font-InterSemiBold text-lg text-grayCustom">
                     Baru saja
                   </Text>
-                  <TouchableOpacity onPress={openModal}>
+                  <Pressable onPress={openModal}>
                     <AntDesign name="closecircle" size={20} />
-                  </TouchableOpacity>
+                  </Pressable>
                 </View>
               )}
               <FlatList
@@ -158,9 +165,7 @@ const SearchAccount = ({ navigation }) => {
           closeModal();
         }}
         title={"Hapus semua pencarian terbaru?"}
-        subtitle={
-          "Tindakan ini menghapus semua penelusuran terbaru"
-        }
+        subtitle={"Tindakan ini menghapus semua penelusuran terbaru"}
         textBtnCancel={"Batal"}
         textBtnOk={"Hapus"}
       />
@@ -168,4 +173,4 @@ const SearchAccount = ({ navigation }) => {
   );
 };
 
-export default SearchAccount;
+export default React.memo(SearchAccount);
