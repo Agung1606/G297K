4ef2d5dfcb5726fa6b2173b45aa6691b75809e8a
Agung1606 <agngsptra928@gmail.com>
2023-07-14 10:14:23 +0700

    change UI design

--- src/components/card/CommentCard.js
+++ src/components/card/CommentCard.js
@@ -1,32 +1,36 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
-import { Avatar, CommentInteraction } from "../common";
+import { Avatar } from "../common";
 
 const CommentCard = ({ item }) => {
   const navigation = useNavigation();
   const goToVisitProfile = () =>
     navigation.navigate("VisitProfileScreen", {
-      param: { username: item.username, userId: item.userId },
+      username: item.username,
+      userId: item.userId,
     });
 
   return (
-    <View className="p-2 flex-row space-x-2">
+    <View className="flex-row space-x-2 px-2 py-4 border-b border-gray-300">
       <Avatar imgUrl={item.profile} size={40} onPress={goToVisitProfile} />
       <View className="flex-1">
-        <View className="bg-gray-200/70 rounded-md p-2">
-          <View className="flex-row justify-between items-center">
-            <Text className="font-InterSemiBold">{item.name}</Text>
-            <Pressable>
-              <MaterialIcons name="more-vert" size={20} />
-            </Pressable>
+        {/* username and date */}
+        <View className="mb-1 flex-row items-center justify-between">
+          <View>
+            <Text className="font-InterSemiBold">{item.username}</Text>
+            <Text className="text-[10px] font-InterRegular text-gray-400">
+              {item.date}
+            </Text>
           </View>
-          <Text className="font-InterLight text-grayCustom">{item.date}</Text>
-          <Text className="font-InterRegular mt-2">{item.comment}</Text>
+          <TouchableOpacity>
+            <MaterialIcons name="more-vert" size={25} />
+          </TouchableOpacity>
         </View>
-        <CommentInteraction numberOfLikes={item.numberOfLikes} />
+        {/* comment */}
+        <Text className="font-RobotoRegular text-[15px]">{item.comment}</Text>
       </View>
     </View>
   );
