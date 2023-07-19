ad75cad3274cd594dcd471b79797a8b6630a4e10
Agung1606 <agngsptra928@gmail.com>
2023-07-19 13:33:30 +0700

    do a little bit improvement

--- src/services/tweet/getTweets.js
+++ src/services/tweet/getTweets.js
@@ -2,13 +2,31 @@ import { FIREBASE_FIRESTORE } from "../../../firebaseConfig";
 import { onSnapshot, query, collection, orderBy } from "firebase/firestore";
 
 const getTweets = (setDataTweets) => {
+  // set up the query
   let queryTweets = query(
     collection(FIREBASE_FIRESTORE, "tweets"),
     orderBy("date", "desc")
   );
-  onSnapshot(queryTweets, (response) => {
-    setDataTweets(response.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-  });
+
+  // set up the listener and error handling
+  const unsubscribe = onSnapshot(
+    queryTweets,
+    (response) => {
+      // mapping data
+      const tweets = response.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setDataTweets(tweets);
+    },
+    (error) => {
+      // error handling
+      console.error("Error fetching tweets: ", error);
+      // Optionally, you can set an error state or show an error message to the user.
+    }
+  );
+
+  return () => unsubscribe();
 };
 
 export default getTweets;
