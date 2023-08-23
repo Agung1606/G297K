aff9adbddec0afc22aff09533f845af3ab82fe1e
Agung1606 <agngsptra928@gmail.com>
2023-08-23 14:12:59 +0700

    move firebase config to environment variable

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,3 +1,13 @@
+import {
+  FIREBASE_APIKEY,
+  FIREBASE_AUTHDOMAIN,
+  FIREBASE_PROJECTID,
+  FIREBASE_STORAGEBUCKET,
+  FIREBASE_MESSAGINGSENDERID,
+  FIREBASE_APPID,
+  FIREBASE_MEASUREMENTID,
+} from "@env";
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -9,13 +19,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAVyuFCiBWEqZGL7EQMQWIdRYWULOks3uc",
-  authDomain: "g297k-dd26d.firebaseapp.com",
-  projectId: "g297k-dd26d",
-  storageBucket: "g297k-dd26d.appspot.com",
-  messagingSenderId: "815240593498",
-  appId: "1:815240593498:web:9612cdb67e2751815cd863",
-  measurementId: "G-EQNY4Y0VJX",
+  apiKey: FIREBASE_APIKEY,
+  authDomain: FIREBASE_AUTHDOMAIN,
+  projectId: FIREBASE_PROJECTID,
+  storageBucket: FIREBASE_STORAGEBUCKET,
+  messagingSenderId: FIREBASE_MESSAGINGSENDERID,
+  appId: FIREBASE_APPID,
+  measurementId: FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
